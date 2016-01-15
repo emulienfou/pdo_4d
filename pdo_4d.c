1da34516f5c8b36c226edf1d6525b591a3d0577f
Thomas Pulzer <t.pulzer@kniel.de>
2016-01-15 13:04:01 +0100

    Cleaning repo from build files

--- pdo_4d.c
+++ pdo_4d.c
@@ -156,16 +156,16 @@ PHP_MINFO_FUNCTION(pdo_4d)
     
     php_info_print_table_start();
     php_info_print_table_header(2, "PDO Driver for 4D" , "enabled");
-    php_info_print_table_row(2, "Version", "0.5");
+    php_info_print_table_row(2, "Version", "0.6");
     php_info_print_table_row(2, "Status", "Beta");
     php_info_print_table_row(2, "Build Date", "$Date: " __DATE__ " " __TIME__ "$");
-    php_info_print_table_row(2, "Core library", "lib4d_sql 0.3 (http://freshmeat.net/projects/lib4d_sql)");
+    php_info_print_table_row(2, "Core library", "lib4d_sql");
     php_info_print_table_row(2, "Provided by ", 
         "4D (http://www.4d.com), and " 
         "Alter Way (http://www.alterway.fr)"
     );
     php_info_print_table_row(2, "Maintainers", 
-        "Thomas Pulzer"
+        "Thomas Pulzer <t.pulzer@kniel.de>"
     );
     php_info_print_table_end();
 
